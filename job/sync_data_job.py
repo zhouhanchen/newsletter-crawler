06000bf2485529f41d9558dccc1e7bc588af8f34
zhouhanchen <zhouchanhen@ilaw.com>
2025-05-02 00:19:18 +0800

    Revert "feat:"
    This reverts commit 0da02d41ebf80e920f68d35839f79fd9de7420ed.

--- job/sync_data_job.py
+++ job/sync_data_job.py
@@ -1,15 +1,8 @@
 from ai_information_data.service import pull_today_data
 from loguru import logger as log
-from ai_information_data import dao
 
 
 async def sync_data_job():
-    log.info('开始执行 sync_data_job')
-    push_log = await dao.get_today_push_log()
-
-    if push_log is not None and push_log.status == 1:
-        log.info('今日已成功推送，不再进行数据拉取')
-        return
     log.info('开始拉取今天的数据')
     await pull_today_data()
     log.info('今天的数据拉取完成')
