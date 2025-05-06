60db1701538090c075abed285db0c30600ba67f7
zhouhanchen <zhouchanhen@ilaw.com>
2025-05-06 10:49:46 +0800

    fix: 逻辑修改

--- job/craw_job.py
+++ job/craw_job.py
@@ -8,22 +8,22 @@ import json
 def check_todo():
     log.info('🤔start execute check_todo job ...')
     current_date = datetime.now()
-    # 计算前一天的日期
-    previous_date = current_date - timedelta(days=1)
-    # 提取年、月、日并转换为整数
-    year = int(previous_date.year)
-    month = int(previous_date.month)
-    day = int(previous_date.day)
-    log.info(f'容错处理前一天数据，{year}{month}{day}')
-    data = {
-        "year": year,
-        "month": month,
-        "day": day
-    }
-    resp = requests. post(url=f'{hongkong_newsletter_host}/aid/test_check_todo', data=json.dumps(data))
-    if resp.status_code != 200:
-        log.warning('❌请求失败，返回状态码: {}'.format(resp.status_code))
-
+    # # 计算前一天的日期
+    # previous_date = current_date - timedelta(days=1)
+    # # 提取年、月、日并转换为整数
+    # year = int(previous_date.year)
+    # month = int(previous_date.month)
+    # day = int(previous_date.day)
+    # log.info(f'容错处理前一天数据，{year}{month}{day}')
+    # data = {
+    #     "year": year,
+    #     "month": month,
+    #     "day": day
+    # }
+    # resp = requests. post(url=f'{hongkong_newsletter_host}/aid/test_check_todo', data=json.dumps(data))
+    # if resp.status_code != 200:
+    #     log.warning('❌请求失败，返回状态码: {}'.format(resp.status_code))
+    #
     year = int(current_date.year)
     month = int(current_date.month)
     day = int(current_date.day)
