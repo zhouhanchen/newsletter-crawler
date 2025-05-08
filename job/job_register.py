b35f4daad33c1fefaae7c6b9434c54cde58297af
zhouhanchen <zhouchanhen@ilaw.com>
2025-05-08 17:54:57 +0800

    fix: 定时任务时间调整

--- job/job_register.py
+++ job/job_register.py
@@ -8,12 +8,12 @@ from job.sync_data_job import sync_data_job
 
 def init_job():
     scheduler = BackgroundScheduler()
-    log.info('register retry_failed_job interval is 1 hours')
-    scheduler.add_job(retry_failed_job, 'interval', seconds=3600, max_instances=10)
+    log.info('register retry_failed_job interval is 20 minutes')
+    scheduler.add_job(retry_failed_job, 'interval', seconds=1200, max_instances=10)
     log.info('register tag_job, interval is 13 minutes')
     scheduler.add_job(tag_job, 'interval', seconds=780)
-    log.info('register check_todo, interval is 1 hours')
-    scheduler.add_job(check_todo, 'interval', seconds=3600)
+    log.info('register check_todo, interval is 30 minutes')
+    scheduler.add_job(check_todo, 'interval', seconds=1800)
     log.info('register sync_data_job, interval is 10 minutes')
     scheduler.add_job(sync_data_job, 'interval', seconds=600)
     scheduler.start()
